c34f94c320ab7b484760ea9bfdde74c4fe9c008c
AhEssam <ah.issam25@gmail.com>
2023-04-05 16:08:47 +0200

    Delete Salla_integration directory

--- Salla_integration/models/payment_method.py
+++
@@ -1,8 +0,0 @@
-from odoo import api, fields, models
-
-
-class SaleOrder(models.Model):
-    _name = "salla.payment.method"
-
-    name = fields.Char('')
-    journal_id = fields.Many2one('account.journal') 
--- Salla_integration/models/res_partner.py
+++
@@ -1,8 +0,0 @@
-from odoo import api, fields, models
-
-class ResPartner(models.Model):
-    _inherit = 'res.partner'
-
-    salla_id = fields.Char('OpenCart ID')
-    branch = fields.Selection([])
-    salla_integration_id = fields.Many2one('integration.salla')
