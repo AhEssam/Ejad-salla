2c53e3bcc67452c1446757bb87f6b54c0f271cde
AhEssam <ah.issam25@gmail.com>
2023-04-05 14:18:13 +0200

    Add files via upload

--- Salla_integration/__manifest__.py
+++ Salla_integration/__manifest__.py
@@ -10,14 +10,18 @@
         to install this module you need to install our salla connector from salla store 
     """,
 
-    'author': "EJAD Digital Solutions | Ahmed Esam",
+    'author': "EJAD Digital Solutions ",
     'website': "https://ejad.sa/",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
-    'version': '0.1',
+    
+    'version': '15.0',
+    'category': 'Sales',
+    'sequence': 14,
+    'price': 150,
+    'currency': "USD",
 
     # any module necessary for this one to work correctly
     'depends': ['base','sale'],
